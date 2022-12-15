1eb548738c59f62abe1f609969a52e14e2e289ab
Elena Lukashova <ele.lukashova@gmail.com>
2022-12-15 14:51:32 +0100

    feat: make attributes parameter optional

--- src/utils/render/render.ts
+++ src/utils/render/render.ts
@@ -1,6 +1,6 @@
 import { Attribute } from './render.types';
 
-const rendered = (element: string, parent: Element, classes: string, attributes: Attribute): HTMLElement => {
+const rendered = (element: string, parent: Element, classes: string, attributes?: Attribute): HTMLElement => {
   const newElement: HTMLElement | null = document.createElement(element);
   parent.append(newElement);
   newElement.classList.add(...classes.split(' ')); //чтобы принимал и один, и несколько классов
