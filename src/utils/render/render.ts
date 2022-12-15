387dbfedc36e8591faacac2fbf77773f7a64d7c8
Elena Lukashova <ele.lukashova@gmail.com>
2022-12-15 10:01:32 +0100

    refactor: change code comment inside render.ts

--- src/utils/render/render.ts
+++ src/utils/render/render.ts
@@ -7,7 +7,7 @@ const rendered: Rendered = (element: string, parent: Element, classes: string, a
     newElement.classList.add(...classes.split(' ')); //чтобы принимал и один, и несколько классов
 
     if (attrData) {
-      //более чем уверена, что эту хрень придется менять(усложнять) в будущем, т.к. она рассчитана на один аттрибут
+      //более чем уверена, что эту функцию придется менять(усложнять) в будущем, т.к. она рассчитана на один аттрибут
       newElement.setAttribute(attrData[0], attrData[1]);
     }
   }
