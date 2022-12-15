9a66fd4d892cc608ab0c057bbc5f9f1cef322f9f
Elena Lukashova <ele.lukashova@gmail.com>
2022-12-15 14:16:03 +0100

    refactor: remove useless code

--- src/utils/render/render.ts
+++ src/utils/render/render.ts
@@ -1,15 +1,13 @@
-import { Rendered, Attribute } from './render.types';
+import { Attribute } from './render.types';
 
-const rendered: Rendered = (element: string, parent: Element, classes: string, attrData?: Attribute): HTMLElement => {
+const rendered = (element: string, parent: Element, classes: string, attrData?: Attribute): HTMLElement => {
   const newElement: HTMLElement | null = document.createElement(element);
-  if (newElement) {
-    parent.appendChild(newElement);
-    newElement.classList.add(...classes.split(' ')); //чтобы принимал и один, и несколько классов
+  parent.appendChild(newElement);
+  newElement.classList.add(...classes.split(' ')); //чтобы принимал и один, и несколько классов
 
-    if (attrData) {
-      //более чем уверена, что эту функцию придется менять(усложнять) в будущем, т.к. она рассчитана на один аттрибут
-      newElement.setAttribute(attrData[0], attrData[1]);
-    }
+  if (attrData) {
+    //более чем уверена, что эту функцию придется менять(усложнять) в будущем, т.к. она рассчитана на один аттрибут
+    newElement.setAttribute(attrData[0], attrData[1]);
   }
   return newElement;
 };
--- src/utils/render/render.types.ts
+++ src/utils/render/render.types.ts
@@ -1,5 +1,3 @@
 //типы, которые мне нужны для задания аттрибутов html элементам
 
 export type Attribute = [string, string]; // например ['src', 'путь к картинке']
-
-export type Rendered = (element: string, parent: Element, classes: string, attrData?: Attribute) => HTMLElement;
