21c8fd5ac04c7c48f2f8794c3c2afb2bbb4abbec
Elena Lukashova <ele.lukashova@gmail.com>
2022-12-16 19:49:51 +0100

    feat: add cards field

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,17 +1,22 @@
+/* eslint-disable import/no-named-as-default */
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import CardsField from '../cards-field/cards-field';
 
 export default class App {
   private readonly header: Header = new Header();
 
   private readonly footer: Footer = new Footer();
 
+  private readonly cardsField: CardsField = new CardsField();
+
   constructor(private readonly rootElement: HTMLElement) {}
 
   public init(): void {
     this.rootElement.classList.add('root'); // добавляю класс к боди для стилей
-    this.rootElement.append(this.header.element, this.footer.element);
+    this.rootElement.append(this.header.element, this.cardsField.element, this.footer.element);
     this.header.render();
     this.footer.render();
+    this.cardsField.render();
   }
 }
---
+++ src/components/cards-field/cards-field.ts
@@ -0,0 +1,20 @@
+import './cards-field.styles.css';
+import BaseComponent from '../base-component/base-component';
+import rendered from '../../utils/render/render';
+import cardsData from '../../assets/json/data';
+import Card from '../card/card';
+
+export default class CardsField extends BaseComponent {
+  constructor() {
+    super('main', 'cards-field cards');
+  }
+
+  public render(): void {
+    const cardsContainer: HTMLElement = rendered('div', this.element, 'cards__container');
+    const card: Card = new Card(cardsContainer);
+    cardsData.products.forEach((data) => {
+      const cardItem = card.render(data);
+      cardsContainer.appendChild(cardItem);
+    });
+  }
+}
