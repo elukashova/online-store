5201708b6e4bf03824725de05c0923e14697f3f9
elukashova <ele.lukashova@gmail.com>
2022-12-17 18:43:45 +0100

    feat: add price and stock filters and create separate class for filters

--- src/components/cards-field/cards-field.ts
+++ src/components/cards-field/cards-field.ts
@@ -1,8 +1,10 @@
+/* eslint-disable max-lines-per-function */
 import './cards-field.styles.css';
 import BaseComponent from '../base-component/base-component';
 import rendered from '../../utils/render/render';
 import cardsData from '../../assets/json/data';
 import Card from '../card/card';
+import Filter from '../filter/filter';
 
 export default class CardsField extends BaseComponent {
   private readonly categories: number[] = [1, 2, 3, 4, 5, 6];
@@ -19,32 +21,23 @@ export default class CardsField extends BaseComponent {
     const buttonsContainer: HTMLElement = rendered('button', filtersContainer, 'filters__btn');
     rendered('button', buttonsContainer, 'filters__btn-reset', 'Reset filters');
     rendered('button', buttonsContainer, 'filters__btn-copy', 'Copy link');
-    const categoryFilter: HTMLElement = rendered('fieldset', filtersContainer, 'filters__category category');
-    rendered('legend', categoryFilter, 'category__legend-1', 'Category');
-    this.categories.forEach((num) => {
-      const inputWrapper: HTMLElement = rendered('div', categoryFilter, 'category__input-wrapper');
-      rendered('input', inputWrapper, `category__input-${num}`, '', {
-        id: `category-${num}`,
-        type: 'checkbox',
-        name: `category-${num}`,
-      });
-      rendered('label', inputWrapper, `category__label-${num}`, `Category${num}`, {
-        for: `category-${num}`,
-      });
-    });
-    const ratingFilter: HTMLElement = rendered('fieldset', filtersContainer, 'filters__rating rating');
-    rendered('legend', ratingFilter, 'rating__legend-1', 'Rating');
-    this.rating.forEach((num) => {
-      const inputWrapper: HTMLElement = rendered('div', ratingFilter, 'rating__input-wrapper');
-      rendered('input', inputWrapper, `rating__input-${num}`, '', {
-        id: `rating-${num}`,
-        type: 'checkbox',
-        name: `rating-${num}`,
-      });
-      rendered('label', inputWrapper, `rating__label-${num}`, 'Rating', {
-        for: `rating-${num}`,
-      });
-    });
+    // фильтр по категории
+    const categoryFilter: Filter = new Filter(filtersContainer, 'Category');
+    const categoryNames: HTMLElement = categoryFilter.renderCheckbox(this.categories, 'category');
+    filtersContainer.append(categoryNames);
+    // фильтра по рейтингу
+    const ratingFilter: Filter = new Filter(filtersContainer, 'Rating');
+    const ratingNames: HTMLElement = ratingFilter.renderCheckbox(this.categories, 'rating');
+    filtersContainer.append(ratingNames);
+    // фильтр по цене
+    const priceFilter: Filter = new Filter(filtersContainer, 'Price');
+    const pricesTitles: HTMLElement = priceFilter.renderInputRange(this.categories, 'price');
+    filtersContainer.append(pricesTitles);
+    // фильтр по стоку
+    const stockFilter: Filter = new Filter(filtersContainer, 'Stock');
+    const stockTitles: HTMLElement = stockFilter.renderInputRange(this.categories, 'stock');
+    filtersContainer.append(stockTitles);
+
     const cardsContainer: HTMLElement = rendered('div', contentContainer, 'cards__container');
     const card: Card = new Card(cardsContainer);
     cardsData.products.forEach((data) => {
