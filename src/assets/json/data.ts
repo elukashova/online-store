c97b4d623615e8a633dcbd7dab1fbacd20e81e0f
elukashova <ele.lukashova@gmail.com>
2022-12-17 13:57:02 +0100

    refactor: change json data

--- src/assets/json/data.ts
+++ src/assets/json/data.ts
@@ -11,13 +11,7 @@ const posters = {
       brand: 'Apple',
       category: 'smartphones',
       thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/1/1.jpg',
-        'https://i.dummyjson.com/data/products/1/2.jpg',
-        'https://i.dummyjson.com/data/products/1/3.jpg',
-        'https://i.dummyjson.com/data/products/1/4.jpg',
-        'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 2,
@@ -31,12 +25,7 @@ const posters = {
       brand: 'Apple',
       category: 'smartphones',
       thumbnail: 'https://i.dummyjson.com/data/products/2/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/2/1.jpg',
-        'https://i.dummyjson.com/data/products/2/2.jpg',
-        'https://i.dummyjson.com/data/products/2/3.jpg',
-        'https://i.dummyjson.com/data/products/2/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 3,
@@ -49,7 +38,7 @@ const posters = {
       brand: 'Samsung',
       category: 'smartphones',
       thumbnail: 'https://i.dummyjson.com/data/products/3/thumbnail.jpg',
-      images: ['https://i.dummyjson.com/data/products/3/1.jpg'],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 4,
@@ -62,13 +51,7 @@ const posters = {
       brand: 'OPPO',
       category: 'smartphones',
       thumbnail: 'https://i.dummyjson.com/data/products/4/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/4/1.jpg',
-        'https://i.dummyjson.com/data/products/4/2.jpg',
-        'https://i.dummyjson.com/data/products/4/3.jpg',
-        'https://i.dummyjson.com/data/products/4/4.jpg',
-        'https://i.dummyjson.com/data/products/4/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 5,
@@ -82,11 +65,7 @@ const posters = {
       brand: 'Huawei',
       category: 'smartphones',
       thumbnail: 'https://i.dummyjson.com/data/products/5/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/5/1.jpg',
-        'https://i.dummyjson.com/data/products/5/2.jpg',
-        'https://i.dummyjson.com/data/products/5/3.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 6,
@@ -99,12 +78,7 @@ const posters = {
       brand: 'APPle',
       category: 'laptops',
       thumbnail: 'https://i.dummyjson.com/data/products/6/thumbnail.png',
-      images: [
-        'https://i.dummyjson.com/data/products/6/1.png',
-        'https://i.dummyjson.com/data/products/6/2.jpg',
-        'https://i.dummyjson.com/data/products/6/3.png',
-        'https://i.dummyjson.com/data/products/6/4.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 7,
@@ -117,12 +91,7 @@ const posters = {
       brand: 'Samsung',
       category: 'laptops',
       thumbnail: 'https://i.dummyjson.com/data/products/7/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/7/1.jpg',
-        'https://i.dummyjson.com/data/products/7/2.jpg',
-        'https://i.dummyjson.com/data/products/7/3.jpg',
-        'https://i.dummyjson.com/data/products/7/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 8,
@@ -136,13 +105,7 @@ const posters = {
       brand: 'Microsoft Surface',
       category: 'laptops',
       thumbnail: 'https://i.dummyjson.com/data/products/8/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/8/1.jpg',
-        'https://i.dummyjson.com/data/products/8/2.jpg',
-        'https://i.dummyjson.com/data/products/8/3.jpg',
-        'https://i.dummyjson.com/data/products/8/4.jpg',
-        'https://i.dummyjson.com/data/products/8/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 9,
@@ -155,13 +118,7 @@ const posters = {
       brand: 'Infinix',
       category: 'laptops',
       thumbnail: 'https://i.dummyjson.com/data/products/9/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/9/1.jpg',
-        'https://i.dummyjson.com/data/products/9/2.png',
-        'https://i.dummyjson.com/data/products/9/3.png',
-        'https://i.dummyjson.com/data/products/9/4.jpg',
-        'https://i.dummyjson.com/data/products/9/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 10,
@@ -174,12 +131,7 @@ const posters = {
       brand: 'HP Pavilion',
       category: 'laptops',
       thumbnail: 'https://i.dummyjson.com/data/products/10/thumbnail.jpeg',
-      images: [
-        'https://i.dummyjson.com/data/products/10/1.jpg',
-        'https://i.dummyjson.com/data/products/10/2.jpg',
-        'https://i.dummyjson.com/data/products/10/3.jpg',
-        'https://i.dummyjson.com/data/products/10/thumbnail.jpeg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 11,
@@ -192,12 +144,7 @@ const posters = {
       brand: 'Impression of Acqua Di Gio',
       category: 'fragrances',
       thumbnail: 'https://i.dummyjson.com/data/products/11/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/11/1.jpg',
-        'https://i.dummyjson.com/data/products/11/2.jpg',
-        'https://i.dummyjson.com/data/products/11/3.jpg',
-        'https://i.dummyjson.com/data/products/11/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 12,
@@ -210,13 +157,7 @@ const posters = {
       brand: 'Royal_Mirage',
       category: 'fragrances',
       thumbnail: 'https://i.dummyjson.com/data/products/12/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/12/1.jpg',
-        'https://i.dummyjson.com/data/products/12/2.jpg',
-        'https://i.dummyjson.com/data/products/12/3.png',
-        'https://i.dummyjson.com/data/products/12/4.jpg',
-        'https://i.dummyjson.com/data/products/12/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 13,
@@ -230,13 +171,7 @@ const posters = {
       brand: 'Fog Scent Xpressio',
       category: 'fragrances',
       thumbnail: 'https://i.dummyjson.com/data/products/13/thumbnail.webp',
-      images: [
-        'https://i.dummyjson.com/data/products/13/1.jpg',
-        'https://i.dummyjson.com/data/products/13/2.png',
-        'https://i.dummyjson.com/data/products/13/3.jpg',
-        'https://i.dummyjson.com/data/products/13/4.jpg',
-        'https://i.dummyjson.com/data/products/13/thumbnail.webp',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 14,
@@ -250,12 +185,7 @@ const posters = {
       brand: 'Al Munakh',
       category: 'fragrances',
       thumbnail: 'https://i.dummyjson.com/data/products/14/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/14/1.jpg',
-        'https://i.dummyjson.com/data/products/14/2.jpg',
-        'https://i.dummyjson.com/data/products/14/3.jpg',
-        'https://i.dummyjson.com/data/products/14/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 15,
@@ -268,13 +198,7 @@ const posters = {
       brand: 'Lord - Al-Rehab',
       category: 'fragrances',
       thumbnail: 'https://i.dummyjson.com/data/products/15/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/15/1.jpg',
-        'https://i.dummyjson.com/data/products/15/2.jpg',
-        'https://i.dummyjson.com/data/products/15/3.jpg',
-        'https://i.dummyjson.com/data/products/15/4.jpg',
-        'https://i.dummyjson.com/data/products/15/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 16,
@@ -287,13 +211,7 @@ const posters = {
       brand: "L'Oreal Paris",
       category: 'skincare',
       thumbnail: 'https://i.dummyjson.com/data/products/16/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/16/1.png',
-        'https://i.dummyjson.com/data/products/16/2.webp',
-        'https://i.dummyjson.com/data/products/16/3.jpg',
-        'https://i.dummyjson.com/data/products/16/4.jpg',
-        'https://i.dummyjson.com/data/products/16/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 17,
@@ -307,12 +225,7 @@ const posters = {
       brand: 'Hemani Tea',
       category: 'skincare',
       thumbnail: 'https://i.dummyjson.com/data/products/17/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/17/1.jpg',
-        'https://i.dummyjson.com/data/products/17/2.jpg',
-        'https://i.dummyjson.com/data/products/17/3.jpg',
-        'https://i.dummyjson.com/data/products/17/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 18,
@@ -326,13 +239,7 @@ const posters = {
       brand: 'Dermive',
       category: 'skincare',
       thumbnail: 'https://i.dummyjson.com/data/products/18/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/18/1.jpg',
-        'https://i.dummyjson.com/data/products/18/2.jpg',
-        'https://i.dummyjson.com/data/products/18/3.jpg',
-        'https://i.dummyjson.com/data/products/18/4.jpg',
-        'https://i.dummyjson.com/data/products/18/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 19,
@@ -345,16 +252,11 @@ const posters = {
       brand: 'ROREC White Rice',
       category: 'skincare',
       thumbnail: 'https://i.dummyjson.com/data/products/19/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/19/1.jpg',
-        'https://i.dummyjson.com/data/products/19/2.jpg',
-        'https://i.dummyjson.com/data/products/19/3.png',
-        'https://i.dummyjson.com/data/products/19/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
     {
       id: 20,
-      title: 'Freckle Treatment Cream- 15gm',
+      title: 'Freckle Treatment Cream',
       description:
         "Fair & Clear is Pakistan's only pure Freckle cream which helpsfade Freckles, Darkspots and pigments. Mercury level is 0%, so there are no side effects.",
       price: 70,
@@ -364,13 +266,7 @@ const posters = {
       brand: 'Fair & Clear',
       category: 'skincare',
       thumbnail: 'https://i.dummyjson.com/data/products/20/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/20/1.jpg',
-        'https://i.dummyjson.com/data/products/20/2.jpg',
-        'https://i.dummyjson.com/data/products/20/3.jpg',
-        'https://i.dummyjson.com/data/products/20/4.jpg',
-        'https://i.dummyjson.com/data/products/20/thumbnail.jpg',
-      ],
+      images: ['../../assets/images/temp-painting.png', '../../assets/images/temp-painting.png'],
     },
   ],
   total: 20,
