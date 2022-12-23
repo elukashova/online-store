31825860647e3efb830551a817214e1a302ba671
Trickypie <klimova.pr@gmail.com>
2022-12-23 22:32:58 +0300

    feat: realise filter changing and resolve conflicts with develop branch

---
+++ src/components/filter/enums.filter.ts
@@ -0,0 +1,7 @@
+const enum RangeTypes {
+  PriceFrom = 'from-price',
+  PriceTo = 'to-price',
+  StockFrom = 'from-stock',
+  StockTo = 'to-stock',
+}
+export default RangeTypes;
--- src/components/filter/filter.ts
+++ src/components/filter/filter.ts
@@ -1,70 +1,152 @@
 import './filter.styles.css';
 import rendered from '../../utils/render/render';
 import cardsData from '../../assets/json/data';
-import findMinAndMaxPrice from './find.minmax.price';
-import findMinAndMaxCount from './find.minmax.stock';
+import findMinAndMaxPrice from './utils/find.minmax.price';
+import findMinAndMaxCount from './utils/find.minmax.stock';
+import RangeTypes from './enums.filter';
+// import filter from './test';
 
 export default class Filter {
   constructor(private readonly container: HTMLElement, private readonly name: string) {}
-  /* параметр name мне нужен только для создания заголовка фильтра,
-  который потом будет выведен на экран */
 
   public renderCheckbox<T>(data: T[], str: string): HTMLElement {
-    // параметр str мне нужен только для создания классов разных элементов
     const filterWrapper: HTMLElement = rendered('fieldset', this.container, `filters__${str} ${str}`);
     rendered('legend', filterWrapper, `${str}__legend-1`, this.name);
     data.forEach((item, ind) => {
       const inputWrapper: HTMLElement = rendered('div', filterWrapper, `${str}__input-wrapper`);
-      rendered('input', inputWrapper, `${str}__input-${ind + 1}`, '', {
-        id: `${str}-${ind + 1}`,
+      const inputCheckbox = rendered('input', inputWrapper, `${str}__input-${ind + 1} ${item}`, '', {
+        id: `${item}`,
         type: 'checkbox',
         name: `${str}-${ind + 1}`,
+        'data-filter': `${item}`,
       });
       rendered('label', inputWrapper, `${str}__label-${ind + 1}`, `${item}`, {
         for: `${str}-${ind + 1}`,
       });
+      rendered('span', inputWrapper, `${str}__out-of-${ind + 1}`, '1/5');
+      this.listenInputCheck(inputCheckbox);
     });
     return filterWrapper;
+  }
+
+  public listenInputCheck(inputCheckbox: HTMLElement): void {
+    inputCheckbox.addEventListener('click', (e) => {
+      if (e.target && e.target instanceof HTMLInputElement) {
+        if (e.target.checked) {
+          if (e.target.dataset.filter) {
+            const cardsContainer: HTMLCollectionOf<Element> = document.getElementsByClassName(
+              `${e.target.dataset.filter}`,
+            );
+            const arr = Array.prototype.slice.call(cardsContainer);
+            console.log(arr);
+            //  filter(e.target.dataset.filter, cardsContainer[]);
+          }
+        }
+      }
+    });
   }
 
   public renderInputRange(str: string): HTMLElement {
     const filterWrapper: HTMLElement = rendered('fieldset', this.container, `filters__${str}`);
     rendered('legend', filterWrapper, `filters__${str}_legend`, this.name);
     const inputWrapper: HTMLElement = rendered('div', filterWrapper, `filters__${str}_wrapper`);
-    const valueWrapper: HTMLElement = rendered('div', inputWrapper, `filters__${str}_value-wrapper ${str}-value`);
-
-    // получаем минимальные и максимальные значения из data.ts
-    const minMaxPrice: number[] = findMinAndMaxPrice(cardsData.products);
-    const [minPrice, maxPrice] = minMaxPrice;
-    const minMaxCount: number[] = findMinAndMaxCount(cardsData.products);
-    const [minCount, maxCount] = minMaxCount;
-
-    /* единственное различие двух range это лэйблы с числом и количеством,
-    поэтому я их сохранию в отдельные переменные */
-    const minValue: string = str === 'price' ? `$ ${minPrice}` : `${minCount}`;
-    const maxValue: string = str === 'price' ? `$ ${maxPrice}` : `${maxCount}`;
-
-    rendered('span', valueWrapper, `${str}-value__from`, minValue);
-    rendered('span', valueWrapper, `${str}-value__to`, maxValue);
     const sliderWrapper: HTMLElement = rendered('div', inputWrapper, `filters__${str}_slider`);
+    const valueWrapper: HTMLElement = rendered('div', inputWrapper, `filters__${str}_value-wrapper ${str}-value`);
+    const [minPrice, maxPrice]: number[] = findMinAndMaxPrice(cardsData.products);
+    const [minCount, maxCount]: number[] = findMinAndMaxCount(cardsData.products);
+    const maxValue: string = str === 'price' ? `${maxPrice}` : `${maxCount}`;
+    const minValue: string = str === 'price' ? `${minPrice}` : `${minCount}`;
+    if (str === 'price') {
+      rendered('span', valueWrapper, `${str}-value__from`, `$ ${minValue}`, { id: `from-${str}-value` });
+      rendered('span', valueWrapper, `${str}-value__to`, `$ ${maxValue}`, { id: `to-${str}-value` });
+    } else {
+      rendered('span', valueWrapper, `${str}-value__from`, `${minValue}`, { id: `from-${str}-value` });
+      rendered('span', valueWrapper, `${str}-value__to`, `${maxValue}`, { id: `to-${str}-value` });
+    }
 
-    // в инпут max мне нужно передать значение без знака доллара
-    const maxV: string = str === 'price' ? maxValue.slice(2) : `${maxCount}`;
-
-    rendered('input', sliderWrapper, `filters__${str}_lowest`, '', {
+    const lowestInput: HTMLElement = rendered('input', sliderWrapper, `filters__${str}_lowest`, '', {
       id: `from-${str}`,
       type: 'range',
-      value: '0',
-      min: '0',
-      max: maxV,
+      min: minValue,
+      max: maxValue,
+      value: minValue,
     });
-    rendered('input', sliderWrapper, `filters__${str}_highest`, '', {
+    const highestInput: HTMLElement = rendered('input', sliderWrapper, `filters__${str}_highest`, '', {
       id: `to-${str}`,
       type: 'range',
-      value: maxV,
-      min: '0',
-      max: maxV,
+      min: minValue,
+      max: maxValue,
+      value: maxValue,
     });
+    this.listenInputRange(lowestInput, highestInput); // вешаем функцию слушатель
     return filterWrapper;
+  }
+
+  public listenInputRange(lowestInput: HTMLElement, highestInput: HTMLElement): void {
+    lowestInput.addEventListener('input', (e) => {
+      if (e.target === document.getElementById('from-price')) {
+        this.changeLowInput(lowestInput, highestInput, RangeTypes.PriceFrom);
+      }
+      if (e.target === document.getElementById('from-stock')) {
+        this.changeLowInput(lowestInput, highestInput, RangeTypes.StockFrom);
+      }
+    });
+    highestInput.addEventListener('input', (e) => {
+      if (e.target === document.getElementById('to-price')) {
+        this.changeHighInput(lowestInput, highestInput, RangeTypes.PriceTo);
+      }
+      if (e.target === document.getElementById('to-stock')) {
+        this.changeHighInput(lowestInput, highestInput, RangeTypes.StockTo);
+      }
+    });
+  }
+
+  public changeLowInput(lowestInput: HTMLElement, highestInput: HTMLElement, id: string): void {
+    const low: HTMLElement = lowestInput;
+    const high: HTMLElement = highestInput;
+    const priceFrom: HTMLElement | null = document.getElementById('from-price-value');
+    const countFrom: HTMLElement | null = document.getElementById('from-stock-value');
+    if (low && high && low instanceof HTMLInputElement && high instanceof HTMLInputElement) {
+      const gap = 1;
+      if (+high.value - +low.value <= gap) {
+        low.value = (+high.value - gap).toString();
+      }
+
+      if (id === RangeTypes.PriceFrom) {
+        if (priceFrom) {
+          priceFrom.textContent = `$ ${low.value}`;
+        }
+      }
+      if (id === RangeTypes.StockFrom) {
+        if (countFrom) {
+          countFrom.textContent = `${low.value}`;
+        }
+      }
+      low.setAttribute('value', `${low.value}`);
+    }
+  }
+
+  public changeHighInput(lowestInput: HTMLElement, highestInput: HTMLElement, id: string): void {
+    const low: HTMLElement = lowestInput;
+    const high: HTMLElement = highestInput;
+    const priceTo: HTMLElement | null = document.getElementById('to-price-value');
+    const countTo: HTMLElement | null = document.getElementById('to-stock-value');
+    if (low && high && low instanceof HTMLInputElement && high instanceof HTMLInputElement) {
+      const gap = 1;
+      if (+high.value - +low.value <= gap) {
+        high.value = (+low.value + gap).toString();
+      }
+      if (id === RangeTypes.PriceTo) {
+        if (priceTo) {
+          priceTo.textContent = `$ ${high.value}`;
+        }
+      }
+      if (id === RangeTypes.StockTo) {
+        if (countTo) {
+          countTo.textContent = `${high.value}`;
+        }
+      }
+      high.setAttribute('value', `${high.value}`);
+    }
   }
 }
