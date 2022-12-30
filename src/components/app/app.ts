c9b155a88830080cd756d59d93d93edc468ca869
elukashova <ele.lukashova@gmail.com>
2022-12-30 15:22:05 +0100

    feat: change router to correctly refresh product pages

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -4,6 +4,7 @@ import { Routes } from './enums';
 import Main from '../main/main-component';
 import CardsField from '../cards-field/cards-field';
 import Cart from '../shopping-cart/shopping-cart';
+import ProductPage from '../product-page/product-page';
 
 export default class App {
   private readonly header: Header;
@@ -14,11 +15,14 @@ export default class App {
 
   private readonly routes: Routes;
 
+  private productID: string = '';
+
+  private component: Element | null = null;
+
   constructor(private readonly rootElement: HTMLElement) {
     this.header = new Header(this.route);
     this.routes = {
-      store: new CardsField(this.header),
-      cart: new Cart(this.header),
+      store: new CardsField(this.header, this.route),
     };
     this.init();
   }
@@ -38,6 +42,11 @@ export default class App {
       window.history.pushState({}, '', e.target.href);
       this.locationHandler();
     }
+    if (e.target instanceof HTMLImageElement) {
+      window.history.pushState({}, '', e.target.id);
+      this.productID = e.target.id;
+      this.locationHandler();
+    }
   };
 
   public locationHandler = async (): Promise<void> => {
@@ -46,29 +55,35 @@ export default class App {
       location = '/';
     }
 
-    let component: Element | null;
+    if (Number(location.slice(1))) {
+      this.productID = location.slice(1);
+    }
 
     switch (location) {
       case '/cart':
         this.routes.cart = new Cart(this.header);
-        component = this.routes.cart.element;
+        this.component = this.routes.cart.element;
         break;
       case '/':
-        this.routes.store = new CardsField(this.header);
-        component = this.routes.store.element;
+        this.routes.store = new CardsField(this.header, this.route);
+        this.component = this.routes.store.element;
         break;
-      default:
-        // строки для теста, будут заменены 404
-        component = document.createElement('div');
-        component.textContent = 'NO PAGE FOUND';
+      // TODO: решить проблему с рефрешем страницы
+      case `/${this.productID}`:
+        this.routes.productPage = new ProductPage(Number(this.productID));
+        this.component = this.routes.productPage.element;
+        break;
+      default: // TODO: строки для теста, будут заменены 404
+        this.component = document.createElement('div');
+        this.component.textContent = 'NO PAGE FOUND';
     }
 
     if (!this.mainContainer.element.hasChildNodes()) {
-      this.mainContainer.setContent(component);
+      this.mainContainer.setContent(this.component);
     } else {
       const child: ChildNode | null = this.mainContainer.element.firstChild;
       if (child) {
-        this.mainContainer.element.replaceChild(component, child);
+        this.mainContainer.element.replaceChild(this.component, child);
       }
     }
   };
--- src/components/app/enums.ts
+++ src/components/app/enums.ts
@@ -1,7 +1,9 @@
 import CardsField from '../cards-field/cards-field';
+import ProductPage from '../product-page/product-page';
 import Cart from '../shopping-cart/shopping-cart';
 
 export interface Routes {
   store: CardsField;
-  cart: Cart;
+  productPage?: ProductPage;
+  cart?: Cart;
 }
