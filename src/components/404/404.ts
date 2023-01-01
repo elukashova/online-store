33375c5999d49950286aa53fc0bcbeb9e1d79316
elukashova <ele.lukashova@gmail.com>
2023-01-01 17:26:41 +0100

    feat: add 404 page

---
+++ src/components/404/404.ts
@@ -0,0 +1,28 @@
+import rendered from '../../utils/render/render';
+import BaseComponent from '../base-component/base-component';
+import './404.styles.css';
+
+export default class Page404 extends BaseComponent {
+  constructor(private callback: (event: Event) => void) {
+    super('div', 'page-not-found__container page-not-found');
+    this.render();
+  }
+
+  private render(): void {
+    const contentWrapper: HTMLElement = rendered('div', this.element, 'page-not-found__content-wrapper');
+    const textWrapper: HTMLElement = rendered('div', contentWrapper, 'page-not-found__text-wrapper');
+    rendered('span', textWrapper, 'page-not-found__title', 'Ooops...');
+    rendered('span', textWrapper, 'page-not-found__text', 'Page not found');
+    rendered('img', contentWrapper, 'page-not-found__background', '', {
+      src: 'assets/images/404-background.png',
+    });
+    const toStoreBtn: HTMLElement = rendered('button', contentWrapper, 'page-not-found__to-store-btn', 'Take me away');
+    toStoreBtn.addEventListener('click', this.toStoreBtnCallback);
+  }
+
+  private toStoreBtnCallback = (e: Event): void => {
+    e.preventDefault();
+    window.location.href = '/';
+    this.callback(e);
+  };
+}
--- src/components/app/app-types.ts
+++ src/components/app/app-types.ts
@@ -1,3 +1,4 @@
+import Page404 from '../404/404';
 import CardsField from '../cards-field/cards-field';
 import ProductPage from '../product-page/product-page';
 import Cart from '../shopping-cart/shopping-cart';
@@ -6,4 +7,5 @@ export interface Routes {
   store: CardsField;
   productPage?: ProductPage;
   cart?: Cart;
+  notfound?: Page404;
 }
