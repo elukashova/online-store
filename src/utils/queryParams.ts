1e84ef84d8a0f0cb518f22ac8258a14994e37848
Trickypie <klimova.pr@gmail.com>
2023-01-08 00:46:33 +0300

    feat: add delete function for separate query parameters

--- src/utils/queryParams.ts
+++ src/utils/queryParams.ts
@@ -12,3 +12,28 @@ export const getQueryParams = (key: string): string | null => {
   const value: string | null = params.get(key);
   return value;
 };
+
+export const getUrl = (): string | null => {
+  const currentUrl = window.location.href;
+  console.log(currentUrl);
+  return currentUrl;
+};
+
+// функция удаления query параметров
+export const deleteQueryParams = (key: string): void => {
+  const params: URLSearchParams = new URLSearchParams(window.location.search);
+  params.delete(key);
+  const newRelativePathQuery = `${window.location.pathname}?${params.toString()}`;
+  window.history.pushState(null, '', newRelativePathQuery);
+};
+
+// функция удаления одного параметра
+export const deleteOneQueryParam = (key: string, value: string): void => {
+  const searchParams = new URLSearchParams(window.location.search);
+  const success = searchParams.getAll(key).toString().split('-');
+  success.splice(success.indexOf(value), 1);
+  const res = success.join('-').toString();
+  searchParams.set(key, res);
+  const newRelativePathQuery = `${window.location.pathname}?${searchParams.toString()}`;
+  window.history.pushState(null, '', newRelativePathQuery);
+};
