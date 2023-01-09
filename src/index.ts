eebad12f35010ddb57642fc423a42c524e19ccbe
elukashova <ele.lukashova@gmail.com>
2023-01-09 19:18:39 +0100

    refactor: add console log with self-evaluation

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,20 @@ import App from './components/app/app';
 
 const app: App = new App(document.body);
 
+const startMessage = `
+Доброго времени суток! Все пункты ТЗ выполнены.
+Балл: 300/300.
+Приятной проверки!
+
+С наилучшими пожеланиями,
+Лена и Настя.
+`;
+
 window.onload = (): void => {
   // добавила зачистку localstorage для удобства тестирования
   // localStorage.clear();
   app.init();
+  console.log(startMessage);
 };
 
 window.onpopstate = (): void => {
