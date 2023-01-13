3646d3c645f514d17fe9881fc6c807a69b14849c
Trickypie <klimova.pr@gmail.com>
2023-01-14 02:58:14 +0300

    refactor: delete remove repetitions, add types, shorten code in filter

--- src/components/filter/utils/find.minmax.ts
+++ src/components/filter/utils/find.minmax.ts
@@ -1,9 +1,12 @@
 import { CardDataType } from '../../card/card.types';
+import { FilterType } from '../enums.filter';
 
 // получаем минимальные и максимальные значения из data.ts
-
 const findMinAndMax = (data: CardDataType[], field: string): number[] => {
-  const valueArray = data.map((card) => (field === 'price' ? card.price : card.stock));
+  // eslint-disable-next-line arrow-body-style
+  const valueArray: number[] = data.map((card): number => {
+    return field === FilterType.Price ? card.price : card.stock;
+  });
   const min: number = Math.min(...valueArray);
   const max: number = Math.max(...valueArray);
   return [min, max];
